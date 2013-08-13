82a1d2727468d537136a061fa9f7d39356a0587a
Pippijn van Steenhoven <pip88nl@gmail.com>
2013-08-13 22:50:14 +0200

    add HTML variable

--- generator.mk
+++ generator.mk
@@ -3,6 +3,9 @@ THIS	:= $(dir $(lastword $(MAKEFILE_LIST)))
 # Generator script and its dependencies.
 DEPEND	:= $(THIS)generator $(shell find $(THIS)lib -type f) config.pm
 
+# All html pages.
+HTML	= $(shell find home -name "*.html")
+
 build: $(DEPEND)
 	$< $(FLAGS)
 	find home -type f -exec chmod 0644 {} ';'
