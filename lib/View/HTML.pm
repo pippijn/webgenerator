a8f939ccd2020f153e2a64ae00e0150d6d662b21
Pippijn van Steenhoven <pip88nl@gmail.com>
2013-08-14 06:30:37 +0200

    xml menu

--- lib/View/HTML.pm
+++ lib/View/HTML.pm
@@ -2,40 +2,52 @@ package View::HTML;
 
 use common::sense;
 
+use XML::Generator (
+   escape => 'unescaped',
+   conformance => 'strict',
+   pretty => 2,
+);
+
 sub menu {
    my ($cur, $data, $path) = @_;
 
-   my $retval = "<ul>\n";
+   my @retval;
    for my $submenu (@$data) {
-      $retval .= "<li>\n";
-      if (@$submenu == 1) {
-         # This branch is only here for the root index.
-         if ($cur eq ${path} . "index") {
-            $retval .= "<a id=\"actmenu\">$submenu->[0]";
-         } else {
-            $retval .= "<a href=\"$path\">$submenu->[0]";
-         }
-      } else {
-         if ($cur eq $path . $submenu->[0]) {
-            $retval .= "<a id=\"actmenu\">$submenu->[1]";
-         } else {
-            $retval .= "<a href=\"$path$submenu->[0]\">$submenu->[1]";
-         }
-      }
+      my ($retval, $menu);
+
       if (@$submenu >= 3) {
          if (   $cur =~ /$submenu->[0]$/ 
              or $cur =~ /$submenu->[0]\//) {
-            $retval .= " <span class=\"small\">[-]</span></a>";
-            $retval .= menu ($cur, $submenu->[2], "$path$submenu->[0]/");
+            $retval = span ({ class => "small" }, "[-]");
+            $menu = menu ($cur, $submenu->[2], "$path$submenu->[0]/");
          } else {
-            $retval .= " <span class=\"small\">[+]</span></a>";
+            $retval = span ({ class => "small" }, "[+]")
          }
-      } else {
-         $retval .= "</a>";
       }
-      $retval .= "</li>";
+
+      if (@$submenu == 1) {
+         $retval = a (
+            ($cur eq $path . "index"
+               # This branch is only here for the root index.
+               ? { id => "actmenu" }
+               : { href => "$path" }),
+            $submenu->[0],
+            $retval
+         )
+      } else {
+         $retval = a (
+            ($cur eq $path . $submenu->[0]
+               ? { id => "actmenu" }
+               : { href => "$path$submenu->[0]" }
+            ),
+            $submenu->[1],
+            $retval
+         )
+      }
+
+      push @retval, li ($retval, $menu);
    }
-   "$retval</ul>"
+   ul (@retval)
 }
 
 
